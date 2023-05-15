54acdf1d7e88a274ba1f8e228a9be289b8d60409
SashaTrincher <oofman@ukr.net>
2023-05-15 19:31:06 +0200

    js change 1

--- script.js
+++ script.js
@@ -6,31 +6,13 @@ const mainBlock = document.querySelector('.main');
 toggle.addEventListener('click', () => {
     if (block.className === 'event-container') {
         block.classList.add('active');
-    } else {
-        block.classList.remove('active');
-    };
-});
-
-toggle.addEventListener('click', () => {
-    if (toggle.className === 'click-event') {
+        mainBlock.classList.add('active');
+        blockText.classList.replace('inactive', 'active');
         toggle.classList.add('active');
     } else {
+        block.classList.remove('active');
         toggle.classList.remove('active');
-    };
-});
-
-toggle.addEventListener('click', () => {
-    if (blockText.className === 'inactive') {
-        blockText.classList.replace('inactive', 'active');
-    } else {
         blockText.classList.replace('active', 'inactive');
-    };
-});
-
-toggle.addEventListener('click', () => {
-    if (mainBlock.className === 'main') {
-        mainBlock.classList.add('active');
-    } else {
         mainBlock.classList.remove('active');
     };
 });
